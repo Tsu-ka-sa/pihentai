53e8470d6e67abd0653919d9b05e0522a649a53f
redpfire <redpixafire@gmail.com>
2019-06-04 21:12:12 +0200

    pihentai.py: Fix 404 issues and introduce sleep

--- pihentai.py
+++ pihentai.py
@@ -1,8 +1,7 @@
 #!/bin/env python3
 
 from mpmath import mp
-import random
-import requests
+import random, time, requests
 
 mp.dps = 10000 # 10000 digits of PI
 
@@ -11,9 +10,10 @@ ind = random.randint(random.randint(0,2000), 6000) # random index
 hentais = [s[i:i+6] for i in range(ind, len(s), 6)]
 
 for hentai in hentais:
+    time.sleep(0.5)
     try:
         r = requests.head("https://nhentai.net/g/%s/" % hentai)
-        if r.status_code is 404:
+        if r.status_code is not 200:
             continue
         print("https://nhentai.net/g/%s/" % hentai)
     except requests.ConnectionError:
